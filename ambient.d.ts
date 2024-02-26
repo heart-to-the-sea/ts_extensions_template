4f62e9b5c2eefbac21fbaf392087a41b46c98840
heart-to-the-sea <x1911898144@163.com>
2024-02-26 17:22:58 +0800

    fix type error

--- ambient.d.ts
+++ ambient.d.ts
@@ -1,3 +1,5 @@
+import '@girs/gtk-4.0'
+import '@girs/adw-1'
 import "@girs/gio-2.0"
 import "@girs/meta-13"
 import "@girs/shell-13"
@@ -7,4 +9,7 @@ import "@girs/gjs/dom"
 import "@girs/gnome-shell/extensions/extension"
 // import "@girs/gnome-shell/dist/index"
 // import "@girs/gnome-shell/dist/index-ambient"
-import "@girs/gnome-shell/dist/ui"
+// import "@girs/gnome-shell/dist/ui"
+import "@girs/gnome-shell/extensions/extension/ambient"
+import "@girs/gnome-shell/extensions/prefs/ambient"
+
